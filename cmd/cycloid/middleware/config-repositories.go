e10a2b4b71e3ce78958297c5ab69e54f3e0c5894
gaelL <contact@gaell.info>
2020-08-27 14:38:14 +0200

    migrate config repo to middleware

--- cmd/cycloid/middleware/config-repositories.go
+++ cmd/cycloid/middleware/config-repositories.go
@@ -57,3 +57,133 @@ func (m *middleware) PushConfig(org string, project string, env string, configs 
 
 	return nil
 }
+
+func (m *middleware) ListConfigRepositories(org string) ([]*models.ConfigRepository, error) {
+
+	params := organization_config_repositories.NewGetConfigRepositoriesParams()
+	params.SetOrganizationCanonical(org)
+
+	resp, err := m.api.OrganizationConfigRepositories.GetConfigRepositories(params, root.ClientCredentials())
+	if err != nil {
+		return nil, err
+	}
+
+	p := resp.GetPayload()
+	// TODO this validate have been removed https://github.com/cycloidio/youdeploy-http-api/issues/2262
+	// err = p.Validate(strfmt.Default)
+	// if err != nil {
+	// 	return err
+	// }
+
+	d := p.Data
+
+	return d, err
+}
+
+func (m *middleware) GetConfigRepository(org string, configRepo uint32) (*models.ConfigRepository, error) {
+
+	params := organization_config_repositories.NewGetConfigRepositoryParams()
+	params.SetOrganizationCanonical(org)
+	params.SetConfigRepositoryID(configRepo)
+
+	resp, err := m.api.OrganizationConfigRepositories.GetConfigRepository(params, root.ClientCredentials())
+
+	if err != nil {
+		return nil, err
+	}
+
+	p := resp.GetPayload()
+	// TODO this validate have been removed https://github.com/cycloidio/youdeploy-http-api/issues/2262
+	// err = p.Validate(strfmt.Default)
+	// if err != nil {
+	// 	return err
+	// }
+
+	d := p.Data
+
+	return d, err
+}
+
+func (m *middleware) DeleteConfigRepository(org string, configRepo uint32) error {
+	params := organization_config_repositories.NewDeleteConfigRepositoryParams()
+	params.SetOrganizationCanonical(org)
+	params.SetConfigRepositoryID(configRepo)
+
+	_, err := m.api.OrganizationConfigRepositories.DeleteConfigRepository(params, root.ClientCredentials())
+	return err
+}
+
+func (m *middleware) CreateConfigRepository(org, name, url, branch string, setDefault bool, cred uint32) (*models.ConfigRepository, error) {
+
+	params := organization_config_repositories.NewCreateConfigRepositoryParams()
+	params.SetOrganizationCanonical(org)
+
+	body := &models.CreateConfigRepository{
+		Branch:       &branch,
+		CredentialID: &cred,
+		Default:      &setDefault,
+		Name:         &name,
+		URL:          &url,
+	}
+
+	params.SetBody(body)
+	err := body.Validate(strfmt.Default)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := m.api.OrganizationConfigRepositories.CreateConfigRepository(params, root.ClientCredentials())
+
+	if err != nil {
+		return nil, err
+	}
+
+	p := resp.GetPayload()
+	// TODO this validate have been removed https://github.com/cycloidio/youdeploy-http-api/issues/2262
+	// err = p.Validate(strfmt.Default)
+	// if err != nil {
+	// 	return err
+	// }
+
+	d := p.Data
+
+	return d, err
+}
+
+func (m *middleware) UpdateConfigRepository(org string, configRepo uint32, name, url, branch string, setDefault bool, cred uint32) (*models.ConfigRepository, error) {
+
+	params := organization_config_repositories.NewUpdateConfigRepositoryParams()
+	params.SetOrganizationCanonical(org)
+	params.SetConfigRepositoryID(configRepo)
+
+	body := &models.UpdateConfigRepository{
+		Branch:       &branch,
+		CredentialID: &cred,
+		Default:      &setDefault,
+		Name:         &name,
+		URL:          &url,
+	}
+
+	params.SetBody(body)
+	err := body.Validate(strfmt.Default)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := m.api.OrganizationConfigRepositories.UpdateConfigRepository(params, root.ClientCredentials())
+
+	if err != nil {
+		return nil, err
+	}
+
+	p := resp.GetPayload()
+	// TODO this validate have been removed https://github.com/cycloidio/youdeploy-http-api/issues/2262
+	// err = p.Validate(strfmt.Default)
+	// if err != nil {
+	// 	return err
+	// }
+
+	d := p.Data
+
+	return d, err
+}
