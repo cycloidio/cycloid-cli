0febc091a42e00a0236b44ee5569bbc988b7e81c
Mathieu Tortuyaux <mathieu.tortuyaux@gmail.com>
2020-08-31 12:38:30 +0200

    printer: add json printer
    this basically print out the []byte

---
+++ printer/json/printer.go
@@ -0,0 +1,25 @@
+package json
+
+import (
+	"encoding/json"
+	"io"
+
+	"github.com/pkg/errors"
+
+	"github.com/cycloidio/youdeploy-cli/printer"
+)
+
+// JSON implements the printer interface
+type JSON struct{}
+
+// Print will write the object in the writer using the given options
+func (j JSON) Print(obj interface{}, opts printer.Options, w io.Writer) error {
+	payload, err := json.Marshal(obj)
+	if err != nil {
+		return errors.Wrap(err, "unable to marshal object")
+	}
+	if _, err = w.Write(payload); err != nil {
+		return errors.Wrap(err, "unable to write JSON in the writer")
+	}
+	return nil
+}
