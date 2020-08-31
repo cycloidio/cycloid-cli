3b36974c0147961123537b58862ab9fb799efa04
Mathieu Tortuyaux <mathieu.tortuyaux@gmail.com>
2020-08-31 13:42:15 +0200

    printer: add factory
    this factory add an helper to get the requested printer

---
+++ printer/factory/factory_test.go
@@ -0,0 +1,31 @@
+package factory
+
+import (
+	"testing"
+
+	"github.com/cycloidio/youdeploy-cli/printer"
+	"github.com/cycloidio/youdeploy-cli/printer/json"
+	"github.com/cycloidio/youdeploy-cli/printer/yaml"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetPrinter(t *testing.T) {
+	t.Run("SuccessYAML", func(t *testing.T) {
+		p, err := GetPrinter("yaml")
+		require.NoError(t, err)
+		assert.Implements(t, (*printer.Printer)(nil), p)
+		assert.IsType(t, yaml.YAML{}, p)
+	})
+	t.Run("SuccessJSON", func(t *testing.T) {
+		p, err := GetPrinter("json")
+		require.NoError(t, err)
+		assert.Implements(t, (*printer.Printer)(nil), p)
+		assert.IsType(t, json.JSON{}, p)
+	})
+	t.Run("ErrorNotSupported", func(t *testing.T) {
+		p, err := GetPrinter("not a printer")
+		require.Nil(t, p)
+		assert.Error(t, err)
+	})
+}
