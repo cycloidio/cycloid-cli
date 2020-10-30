a3bca7538264ab1f6622b9bc555e87b9d975a4ca
Mathieu Tortuyaux <mathieu.tortuyaux@gmail.com>
2020-10-30 18:06:36 +0100

    cmd/cycloid: add `api-key` subcommand
    use to manage Cycloid organization API keys

---
+++ cmd/cycloid/apikey/cmd.go
@@ -0,0 +1,30 @@
+package apikey
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/cycloidio/cycloid-cli/cmd/cycloid/common"
+)
+
+var (
+	example = `
+	# Manage API keys of my-org organization
+	cy --org my-org api-key [create|list|get|delete]
+`
+	short = "Manage organization API keys"
+)
+
+func NewCommands() *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "api-key",
+		Aliases: []string{
+			"api-keys",
+			"ak",
+		},
+		Example: example,
+		Short:   short,
+		Hidden:  true,
+	}
+	common.RequiredPersistentFlag(common.WithFlagOrg, cmd)
+	return cmd
+}
