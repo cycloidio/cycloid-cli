49e22ea2bea9f8bfab4c7fcb59b5305e0863c276
Łukasz Opasiak <lukasz.opasiak@proton.me>
2024-04-15 16:16:02 +0200

    changelog_unreleased: add entry for #254

--- internal/ptr/ptr.go
+++ internal/ptr/ptr.go
@@ -1,10 +1,5 @@
 package ptr
 
-import "github.com/go-openapi/strfmt"
-
-// Email returns the address of a literal strfmt.Email
-func Email(s string) *strfmt.Email { e := strfmt.Email(s); return &e }
-
 // Ptr returns a pointer address of given value
 func Ptr[V any](v V) *V { return &v }
 
