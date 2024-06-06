42929f37302181869aa0318d7fc76541bfb76e1b
Frédéric BARRAS HAMPEL <frederic.hampel@cycloid.io>
2024-06-06 15:33:54 +0200

    misc: add nixos dev env shinanigan

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A basic dev shell for nix users";
+
+  inputs = {
+    nixpkgs = { url = "https://github.com/NixOS/nixpkgs/archive/9957cd48326fe8dbd52fdc50dd2502307f188b0d.tar.gz"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
+        swaggerPython = (pkgs.python312.withPackages (p: with p; [
+          pyyaml
+        ]));
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ swaggerPython ]
+            ++ (with pkgs; [
+            # You packages here
+            gnumake
+
+            # Prod is built using go 1.18 rn
+            go_1_18
+          ]);
+        };
+      });
+}
