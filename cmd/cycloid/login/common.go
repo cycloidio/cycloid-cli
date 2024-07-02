3ab0773fd251cc7b00b8ff0d25da49c75ffee0c1
Frédéric BARRAS HAMPEL <frederic.hampel@cycloid.io>
2024-07-02 15:33:49 +0200

    fix: remove last required org flags

--- cmd/cycloid/login/common.go
+++ cmd/cycloid/login/common.go
@@ -3,18 +3,12 @@ package login
 import "github.com/spf13/cobra"
 
 var (
-	apiKeyFlag   string
-	orgFlag      string
+	apiKeyFlag string
+	orgFlag    string
 )
 
 func WithFlagAPIKey(cmd *cobra.Command) string {
 	flagName := "api-key"
 	cmd.Flags().StringVar(&apiKeyFlag, flagName, "", "API key")
-	return flagName
-}
-
-func WithFlagOrg(cmd *cobra.Command) string {
-	flagName := "org"
-	cmd.Flags().StringVar(&orgFlag, flagName, "", "Org cannonical name")
 	return flagName
 }
