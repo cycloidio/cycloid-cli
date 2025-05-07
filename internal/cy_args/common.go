cd545c3e5f8f69f2f300d6c4e6b0b0b307847d2a
fhacloid <148750436+fhacloid@users.noreply.github.com>
2025-05-07 22:29:14 +0200

    Fix/cy context flags (#341)
    * func: add beta cmd section
    
    * func: add cy beta config commands
    
    * func: add interpolate forms command
    
    * func: add experimental new flag handling
    
    the idea here is to try to find a maintainable pattern for common flags
    inside the CLI.
    
    * func: add components to the middleware
    
    * func: add build for macos and windows
    
    * func: update go to 1.24.1
    
    * fix: missing component argument in middleware
    
    * func: add support for components
    
    * fix: make cy context flag more composable
    
    * rename folder to be more coherent
    
    * misc: save testcontainers
    
    * misc: remove testscontainers
    
    * func: add optimized docker compose
    
    * func: add routes for init first org for tests
    
    * misc: add missing bin to dev env
    
    * fixup! func: add optimized docker compose
    
    * save: compose
    
    * wip: tests
    
    * misc: add .env
    
    * Bump swagger client to version v6.0.98
    
    * Changelog: Add entry for new version v6.0.98
    
    * Changelog: Add entry for new version v6.0.69
    
    * func: moved org/project/env/component testing at middleware level, fixed
    routes
    
    * func: refactor flags management
    
    * misc: fix flakey test
    
    * func: update makefile to use remote test setup
    
    * misc: remove dead code
    
    * fix: error msg
    
    * fix: env var
    
    * misc: add changelogs
    
    * misc: remove useless compose
    
    * misc: update readme
    
    * func: add e2e tests on components
    
    * fix: update golangcilint comfig
    
    * misc: add missing components tests
    
    * misc: rename component list cmd file
    
    * fix: cy context flags not working properly
    
    * fix: bad merge
    
    ---------
    
    Co-authored-by: Cycloid <devops@cycloid.io>

--- internal/cy_args/common.go
+++ internal/cy_args/common.go
@@ -2,6 +2,7 @@ package cy_args
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -13,29 +14,23 @@ func AddCyContext(cmd *cobra.Command) {
 	AddComponentFlag(cmd)
 }
 
-// GetOrg will return the current org using the env_var < flag precedence
-func GetOrg(cmd *cobra.Command) (org string, err error) {
-	org = viper.GetString("org")
-	if org == "" {
-		return "", errors.New("org is not set, use --org flag or CY_ORG env var")
-	}
-
-	return org, nil
-}
+var (
+	v = viper.GetViper()
+)
 
 func AddProjectFlag(cmd *cobra.Command) {
 	cmd.Flags().StringP("project", "p", "", "the project canonical, can also be set with the CY_PROJECT env var")
-	viper.BindPFlag("project", cmd.Flags().Lookup("project"))
+	v.BindPFlag("project", cmd.Flags().Lookup("project"))
 }
 
 func AddEnvFlag(cmd *cobra.Command) {
 	cmd.Flags().StringP("env", "e", "", "the env canonical, can also be set with the CY_ENV env var")
-	viper.BindPFlag("env", cmd.Flags().Lookup("env"))
+	v.BindPFlag("env", cmd.Flags().Lookup("env"))
 }
 
 func AddComponentFlag(cmd *cobra.Command) {
 	cmd.Flags().StringP("component", "c", "", "the component canonical, can also be set with the CY_COMPONENT env var")
-	viper.BindPFlag("component", cmd.Flags().Lookup("component"))
+	v.BindPFlag("component", cmd.Flags().Lookup("component"))
 }
 
 func AddColorFlag(cmd *cobra.Command) {
@@ -54,24 +49,37 @@ func GetCyContext(cmd *cobra.Command) (org, project, env, component string, err 
 
 	project, err = GetProject(cmd)
 	if err != nil {
-		return "", "", "", "", err
+		return org, "", "", "", err
 	}
 
 	env, err = GetEnv(cmd)
 	if err != nil {
-		return "", "", "", "", err
+		return org, project, "", "", err
 	}
 
 	component, err = GetComponent(cmd)
 	if err != nil {
-		return "", "", "", "", err
+		return org, project, env, "", err
 	}
 
 	return org, project, env, component, nil
 }
 
-func GetProject(cmd *cobra.Command) (project string, err error) {
-	project = viper.GetString("project")
+func GetOrg(cmd *cobra.Command) (string, error) {
+	org := v.GetString("org")
+	if org == "" {
+		return "", fmt.Errorf("org is not set, use --org flag or CY_ORG env var, value: %s", org)
+	}
+	return org, nil
+}
+
+func GetProject(cmd *cobra.Command) (string, error) {
+	project, _ := cmd.Flags().GetString("project")
+	if project != "" {
+		return project, nil
+	}
+
+	project = v.GetString("project")
 	if project == "" {
 		return "", errors.New("project is not set, use --project flag or CY_PROJECT env var")
 	}
@@ -79,8 +87,13 @@ func GetProject(cmd *cobra.Command) (project string, err error) {
 	return project, nil
 }
 
-func GetEnv(cmd *cobra.Command) (env string, err error) {
-	env = viper.GetString("env")
+func GetEnv(cmd *cobra.Command) (string, error) {
+	env, _ := cmd.Flags().GetString("env")
+	if env != "" {
+		return env, nil
+	}
+
+	env = v.GetString("env")
 	if env == "" {
 		return "", errors.New("env is not set, use --env flag or CY_ENV env var")
 	}
@@ -88,10 +101,15 @@ func GetEnv(cmd *cobra.Command) (env string, err error) {
 	return env, nil
 }
 
-func GetComponent(cmd *cobra.Command) (component string, err error) {
-	component = viper.GetString("component")
+func GetComponent(cmd *cobra.Command) (string, error) {
+	component, _ := cmd.Flags().GetString("component")
+	if component != "" {
+		return component, nil
+	}
+
+	component = v.GetString("component")
 	if component == "" {
-		return "", errors.New("component is not set, use --component flag or CY_COMPONENT env var")
+		return "", errors.New("component is not set, use --component flag or CY_COMPONENT component var")
 	}
 
 	return component, nil
