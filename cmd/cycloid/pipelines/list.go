802ec20b976c65870688b70470fd5309eebb65ad
Frédéric BARRAS HAMPEL <frederic.hampel@cycloid.io>
2025-05-21 23:09:04 +0200

    func: add pipeline list cmd

--- cmd/cycloid/pipelines/list.go
+++ cmd/cycloid/pipelines/list.go
@@ -1,42 +1,63 @@
 package pipelines
 
 import (
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 
+	"github.com/cycloidio/cycloid-cli/cmd/cycloid/common"
 	"github.com/cycloidio/cycloid-cli/cmd/cycloid/internal"
+	"github.com/cycloidio/cycloid-cli/cmd/cycloid/middleware"
+	"github.com/cycloidio/cycloid-cli/internal/cyargs"
+	"github.com/cycloidio/cycloid-cli/printer"
+	"github.com/cycloidio/cycloid-cli/printer/factory"
 )
 
 func NewListCommand() *cobra.Command {
 	var cmd = &cobra.Command{
 		Use:     "list",
 		Short:   "list all pipelines of the organization",
+		Args:    cobra.NoArgs,
 		PreRunE: internal.CheckAPIAndCLIVersion,
-		// RunE:    list,
-		RunE: func(cmd *cobra.Command, args []string) error { panic("TODO: not implemented") },
+		RunE:    list,
 	}
 
+	cyargs.AddProjectFlag(cmd)
+	cyargs.AddEnvFlag(cmd)
+	cyargs.AddPipelineStatuses(cmd)
+	cyargs.AddPipelineName(cmd)
 	return cmd
 }
 
-// func list(cmd *cobra.Command, args []string) error {
-// 	api := common.NewAPI()
-// 	m := middleware.NewMiddleware(api)
-//
-// 	org, err := cy_args.GetOrg(cmd)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	output, err := cmd.Flags().GetString("output")
-// 	if err != nil {
-// 		return errors.Wrap(err, "unable to get output flag")
-// 	}
-//
-// 	// fetch the printer from the factory
-// 	p, err := factory.GetPrinter(output)
-// 	if err != nil {
-// 		return errors.Wrap(err, "unable to get printer")
-// 	}
-//
-// 	pps, err := m.ListPipelines(org)
-// 	return printer.SmartPrint(p, pps, err, "unable to list pipelines", printer.Options{}, cmd.OutOrStdout())
-// }
+func list(cmd *cobra.Command, args []string) error {
+	api := common.NewAPI()
+	m := middleware.NewMiddleware(api)
+
+	org, err := cyargs.GetOrg(cmd)
+	if err != nil {
+		return err
+	}
+
+	// Thoses args are filter and optionnal
+	project, _ := cyargs.GetProject(cmd)
+	env, _ := cyargs.GetEnv(cmd)
+	statuses, _ := cyargs.GetPipelineStatuses(cmd)
+	pipelineName, _ := cyargs.GetPipelineName(cmd)
+
+	output, err := cmd.Flags().GetString("output")
+	if err != nil {
+		return errors.Wrap(err, "unable to get output flag")
+	}
+
+	// fetch the printer from the factory
+	p, err := factory.GetPrinter(output)
+	if err != nil {
+		return errors.Wrap(err, "unable to get printer")
+	}
+
+	pps, err := m.GetOrgPipelines(org, &pipelineName, &project, &env, statuses)
+	if err != nil {
+		return printer.SmartPrint(p, nil, err, "unable to list pipelines", printer.Options{}, cmd.OutOrStderr())
+	}
+
+	return printer.SmartPrint(p, pps, nil, "", printer.Options{}, cmd.OutOrStdout())
+}
